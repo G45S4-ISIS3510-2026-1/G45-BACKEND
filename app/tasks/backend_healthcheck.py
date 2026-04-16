9b97612517cf20c4fbb1844aa7388a05e14463df
Nicolás Ballén <n.ballen1@uniandes.edu.co>
2026-04-16 13:06:21 -0500

    backend_healthcheck.py novelty

--- app/tasks/backend_healthcheck.py
+++ app/tasks/backend_healthcheck.py
@@ -2,12 +2,21 @@ from apscheduler.schedulers.blocking import BlockingScheduler
 from firebase_admin import messaging
 
 from app.core.firebase import get_firestore_client
+from app.models.novelty import Novelty
+from app.repositories.novelty_repository import NoveltiesRepository
 from app.repositories.user_repository import UserRepository
 async def health_check():
     db           = get_firestore_client()
     user_repo    = UserRepository(db)
+    novelty_repo= NoveltiesRepository(db)
     print("Executing task/notification every 5 minutes...")
     users= await user_repo.get_all()
+    novelty=Novelty(
+        title="Health Check",
+        description="This is a periodic health check notification.",
+        user_id="system",
+        type="HEALTH_CHECK"
+    )
     for user in users:
         if user.fcm_tokens:
             message = messaging.MulticastMessage(
